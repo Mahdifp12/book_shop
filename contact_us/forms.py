6189a8967c3368d406249f335ba61d6cac7733fb
Mahdifp12 <mhdyflah78@gmail.com>
2023-01-01 00:13:50 +0330

    Edit forms.py for add attrs in widget in field form & edit CSS in form-style.css & edit contact_us.html add div for input form

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -2,10 +2,38 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField(max_length=200, label="نام شما", error_messages={
-        "required": "لطفا نام خود را وارد کنید",
-        "max_length": "تعداد حروف نام شما بالاتر از حد مجاز است"
-    })
-    email = forms.EmailField(max_length=200, label="ایمیل شما", widget=forms.EmailInput)
-    title = forms.CharField(max_length=200, label="عنوان پیغام شما")
-    message = forms.CharField(widget=forms.Textarea, label="پیغام شما")
+    full_name = forms.CharField(max_length=100,
+                                label="نام شما",
+                                error_messages={
+                                    "required": "لطفا نام خود را وارد کنید",
+                                    "max_length": "تعداد حروف نام شما بالاتر از حد مجاز است"
+                                },
+                                widget=forms.TextInput(attrs={
+                                    "class": "form-group",
+                                    "placeholder": "نام شما"
+                                })
+                                )
+    email = forms.EmailField(max_length=200,
+                             label="ایمیل شما",
+                             error_messages={
+                                 "required": "لطفا ایمیل خود را وارد کنید"
+                             },
+                             widget=forms.EmailInput(attrs={
+                                 "class": "form-group",
+                                 "placeholder": "ایمیل شما"
+                             }
+                             )
+                             )
+    title = forms.CharField(max_length=200,
+                            label="عنوان پیغام شما",
+                            widget=forms.TextInput(attrs={
+                                "class": "form-group",
+                                "placeholder": "عنوان پیغام شما"
+                            })
+                            )
+    message = forms.CharField(label="پیغام شما",
+                              widget=forms.Textarea(attrs={
+                                  "class": "form-group",
+                                  "placeholder": "پیغام شما"
+                              })
+                              )
