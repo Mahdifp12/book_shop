e4d5b3e94b09cf93c4d1cdad5dd79ded3bdbbcfe
Mahdifp12 <mhdyflah78@gmail.com>
2023-01-31 15:42:04 +0330

    add FormView in ContactUsView

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,37 +1,13 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
-from django.views import View
+from django.views.generic.edit import FormView
 
 from .forms import ContactUsModelForm
 
 
-class ContactUsView(View):
-    def get(self, request):
-        form = ContactUsModelForm()
+class ContactUsView(FormView):
+    template_name = "contact_us/contact_us.html"
+    form_class = ContactUsModelForm
+    success_url = "/"
 
-        return render(
-            request,
-            template_name="contact_us/contact_us.html",
-            context={
-                "form": form
-            }
-        )
-
-    def post(self, request):
-        data = request.POST
-        form = ContactUsModelForm(data=data)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('home-page'))
-
-        form = ContactUsModelForm()
-
-        return render(
-            request,
-            template_name="contact_us/contact_us.html",
-            context={
-                "form": form
-            }
-        )
+    def form_valid(self, form):
+        form.save()
+        return super(ContactUsView, self).form_valid(form)
