6b5295eaa16124cc841da08420e15bc7c1265ee5
Mahdifp12 <mhdyflah78@gmail.com>
2023-01-31 15:46:34 +0330

    Reformated home_module/urls.py

--- home_module/urls.py
+++ home_module/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name="home-page"),
-    # path('site-header', views.site_header_component, name="site_header"),
-    # path('site-footer', views.site_footer_component, name="site_footer"),
-    # path('site-loginform', views.site_loginform_component, name="site_login_form"),
-    # path('site-navbar', views.site_navbar_component, name="site_navbar"),
-    # path('site-head-link', views.site_head_link_component, name="site-head-link"),
-    # path('site-scriptjs', views.site_script_reference, name="site-script-js"),
+
 ]
