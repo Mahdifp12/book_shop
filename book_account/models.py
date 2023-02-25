923448ba3e56a7883703c0191570163ddc03c8dd
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-02-25 17:58:45 +0330

    makemigrations and migrate because add fiald avatar instead mobile fiald

--- book_account/models.py
+++ book_account/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    mobile = models.CharField(max_length=20, verbose_name="شماره موبایل")
+    avatar = models.CharField(max_length=20, verbose_name="تصویر آواتار", null=True, blank=True)
     email_active_code = models.CharField(max_length=100, verbose_name="کد فعالسازی ایمیل")
 
     class Meta:
