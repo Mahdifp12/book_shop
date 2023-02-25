0f71ca1312d9e12294a765b2b8a21761fce28187
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-02-25 17:38:57 +0330

    add logical sign up in RegisterView

--- book_account/views.py
+++ book_account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import RegisterForm
-
+from .models import User
+from django.utils.crypto import get_random_string
 
 class RegisterView(View):
     def get(self, request):
@@ -17,7 +18,23 @@ class RegisterView(View):
         register_form = RegisterForm(request.POST or None)
 
         if register_form.is_valid():
-            print(register_form.cleaned_data)
+            user_email = register_form.cleaned_data.get("email")
+            user_password = register_form.cleaned_data.get("password")
+
+            user: bool = User.objects.filter(email__iexact=user_email).exists()
+
+            if user:
+                register_form.add_error("email", "ایمیل وارد شده تکراری میباشد")
+
+            else:
+                new_user = User(
+                    email=user_email,
+                    email_active_code=get_random_string(length=72),
+                    is_active=False,
+                    username=user_email
+                )
+                new_user.set_password(user_password)
+                new_user.save()
 
         context = {
             "form": register_form
