961b9fc5358ce6ad9030b5e591c4f063f5f7349a
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-03-13 23:08:54 +0330

    add EMAIL HOST or EMAIL BACKEND ...etc in settings.py

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -137,3 +137,15 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ALLOW_UNICODE_SLUGS = True
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_HOST_USER = ""
+
+EMAIL_HOST_PASSWORD = ""
+
+EMAIL_PORT = 587
