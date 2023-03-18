9c62fbe793388aa114ba51f0233b72bba4a1e4f7
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-03-18 22:32:35 +0330

    add path for logout system

--- book_account/urls.py
+++ book_account/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name="register-page"),
     path('login', views.LoginView.as_view(), name="login-page"),
+    path('logout', views.LogoutView.as_view(), name="logout-page"),
     path('forget-password', views.ForgetPassword.as_view(), name="forget-password-page"),
     path('reset-password/<active_code>', views.ResetPassword.as_view(), name="reset-password-page"),
     path('activate-account/<email_active_code>', views.ActivateAccount.as_view(), name="activate-account")
