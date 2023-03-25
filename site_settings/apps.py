05594b703cbcffc940772bbd2692772dd322bd28
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-03-25 13:20:01 +0330

    deleted site_settings module

--- site_settings/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SiteSettingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_settings'
-    verbose_name = "تنظیمات سایت"
