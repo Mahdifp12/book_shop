9d5819ca48923df2b815aa0844bfb76d8aab585e
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-03-30 02:09:49 +0330

    modified for register to panel admin

--- site_settings/admin.py
+++ site_settings/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from . import models
+
+
 # Register your models here.
 
+class FooterLinksAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'url'
+    ]
+
+
 admin.site.register(models.SiteSettings)
+admin.site.register(models.FooterLinkBox)
+admin.site.register(models.FooterLinks)
