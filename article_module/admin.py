ed17c68b23010aaee7e2cca1a8af589dcf02c9f8
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-05 00:36:53 +0330

    REFACTOR & CREATE : new register for article category & create model admin for article category

--- article_module/admin.py
+++ article_module/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'parent', 'is_active']
+    list_editable = ['parent', 'is_active']
+
+
+admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
