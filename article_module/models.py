9e72b4e8c92c3663c1ad343793498d809e5f6dd8
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-05 01:02:46 +0330

    REFACTOR : create a new model (Article)

--- article_module/models.py
+++ article_module/models.py
@@ -21,3 +21,19 @@ class ArticleCategory(models.Model):
     def __str__(self):
         return self.title
 
+
+class Article(models.Model):
+    title = models.CharField(max_length=250, verbose_name="عنوان مقاله")
+    slug = models.SlugField(max_length=400, unique=True, allow_unicode=True, verbose_name="عنوان مقاله در URL")
+    image = models.ImageField(upload_to="images/articles/", verbose_name="تصویر مقاله")
+    short_description = models.TextField(verbose_name="توضیحات کوتاه")
+    text = models.TextField(verbose_name="متن مقاله")
+    is_active = models.BooleanField(default=True, verbose_name="فعال / غیر فعال")
+    selected_categories = models.ManyToManyField(ArticleCategory, verbose_name="دسته بندی ها")
+
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
+
+    def __str__(self):
+        return self.title
