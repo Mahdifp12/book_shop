5c087d0a34f2da15f62314d636647a3c6567dc91
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-05 00:33:29 +0330

    CREATE : new model (ArticleCategory)

--- article_module/models.py
+++ article_module/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class ArticleCategory(models.Model):
+    title = models.CharField(max_length=250, verbose_name="عنوان دسته بندی")
+    url_title = models.CharField(max_length=250, unique=True, verbose_name="عنوان دسته بندی url")
+    parent = models.ForeignKey('ArticleCategory',
+                               null=True,
+                               blank=True,
+                               on_delete=models.CASCADE,
+                               verbose_name="دسته بندی والد")
+    is_active = models.BooleanField(default=True, verbose_name="فعال / غیر فعال")
+
+    class Meta:
+        verbose_name = "دسته بندی مقاله"
+        verbose_name_plural = "دسته بندی های مقاله"
+
+    def __str__(self):
+        return self.title
+
