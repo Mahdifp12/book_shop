98109244fcaec5ee8e371174c45fb9c11fb72798
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-05 00:37:16 +0330

    REFACTOR : add verbose name

--- book_account/apps.py
+++ book_account/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BookAccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'book_account'
+    verbose_name = "ماژول حساب و کاربران"
