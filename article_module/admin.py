4104c42dbf26f27486ae74ce14aae9e44bdeaa2e
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-06 22:19:46 +0330

    REFACTOR : refactoring with pep

--- article_module/admin.py
+++ article_module/admin.py
@@ -5,13 +5,27 @@ from . import models
 # Register your models here.
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'parent', 'is_active']
-    list_editable = ['parent', 'is_active']
+    list_display = [
+        'title',
+        'parent',
+        'is_active'
+    ]
+    list_editable = [
+        'parent',
+        'is_active'
+    ]
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'is_active', 'slug']
-    list_editable = ['is_active', 'slug']
+    list_display = [
+        'title',
+        'is_active',
+        'slug'
+    ]
+    list_editable = [
+        'is_active',
+        'slug'
+    ]
 
 
 admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
