466b7b7e8b3f3eea728fa686f72c5502d8262ce7
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-08 12:45:35 +0330

    REFACTOR : i added a class based view with ListView for articles

--- article_module/views.py
+++ article_module/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
+
 
 # Create your views here.
+
+class ArticlesView(ListView):
+    model = Article
+    paginate_by = 4
+    template_name = ""
