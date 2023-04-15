b03800852843b17b0da794a3f8d5cb99ef2a50df
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-15 11:12:41 +0330

    REFACTOR : i'm optimized condition

--- book_account/models.py
+++ book_account/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
         verbose_name_plural = "کاربر ها"
 
     def __str__(self):
-        if self.first_name is not "" and self.last_name is not "":
+        if self.first_name and self.last_name:
             return self.get_full_name()
 
         return self.email
