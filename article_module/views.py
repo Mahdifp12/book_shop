a6b8647ee3bae1828f038b616aca095b7dd22e6d
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-21 14:00:07 +0330

    CREATE & ADD : i'm created and added a new function based for articles categoried component

--- article_module/views.py
+++ article_module/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from django.views.generic import ListView
 from jalali_date import datetime2jalali, date2jalali
-from .models import Article
+from .models import Article, ArticleCategory
 
 
 # Create your views here.
@@ -14,3 +15,12 @@ class ArticlesView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super(ArticlesView, self).get_context_data(*args, **kwargs)
         return context
+
+
+def article_categories_components(request: HttpRequest):
+    article_main_categories = ArticleCategory.objects.filter(is_active=True, parent_id=None)
+
+    context = {
+        "article_main_categories": article_main_categories
+    }
+    return render(request, "article_module/components/article_categories_component.html", context)
