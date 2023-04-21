649f4f15c34438a6807a8a4e4965f9697a8eebd1
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-21 14:49:53 +0330

    ADD & CHANGE : i'm added a new URL path for article categories filter

--- article_module/urls.py
+++ article_module/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticlesView.as_view(), name="articles-page"),
+    path('category/<str:category>', views.ArticlesView.as_view(), name="articles-by-categories-page"),
 ]
