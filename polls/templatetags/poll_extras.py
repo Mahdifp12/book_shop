dfbcaed0f85e78f5a1fdafbab0cb64b256b1e3af
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-22 14:44:17 +0330

    ADD : i'm added a new function for set jalali time

--- polls/templatetags/poll_extras.py
+++ polls/templatetags/poll_extras.py
@@ -1,5 +1,5 @@
 from django import template
-from jalali_date import date2jalali
+from jalali_date import date2jalali, datetime2jalali
 
 register = template.Library()
 
@@ -7,3 +7,9 @@ register = template.Library()
 @register.filter(name="d_jalali")
 def jalali_date(value):
     return date2jalali(value)
+
+
+@register.filter(name="t_jalali")
+def jalali_time(value):
+    time = datetime2jalali(value).strftime("%H:%M")
+    return time
