6192f42d17eca64b486fa776be5567e46936b402
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-22 14:40:17 +0330

    ADD & CHANGE : i'm added a new URL path for article detail page

--- article_module/urls.py
+++ article_module/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ArticlesView.as_view(), name="articles-page"),
-    path('category/<str:category>', views.ArticlesView.as_view(), name="articles-by-categories-page"),
+    path('', views.ArticleListView.as_view(), name="articles-page"),
+    path('category/<str:category>', views.ArticleListView.as_view(), name="articles-by-categories-page"),
+    path('<int:pk>', views.ArticleDetailView.as_view(), name="article_detail_page"),
 ]
