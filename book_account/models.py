e54e92c63554ffd0b9e7c93d301bf25addbdcdf3
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-04-24 16:30:35 +0330

    REFACTOR : i'm refactoring user model

--- book_account/models.py
+++ book_account/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    avatar = models.CharField(max_length=20, verbose_name="تصویر آواتار", null=True, blank=True)
+    avatar = models.ImageField(upload_to="images/users_avatar", verbose_name="تصویر آواتار", null=True, blank=True)
     email_active_code = models.CharField(max_length=100, verbose_name="کد فعالسازی ایمیل")
     about_user = models.TextField(verbose_name="درباره کاربر", null=True, blank=True)
 
