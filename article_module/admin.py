0ae759e76df9891c8288b3188a4ab5b65caabddb
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-05-01 15:59:54 +0330

    ADD : i'm added list editable and list filter

--- article_module/admin.py
+++ article_module/admin.py
@@ -45,6 +45,17 @@ class ArticleCommentAdmin(admin.ModelAdmin):
         'parent',
     ]
 
+    list_editable = [
+        'parent',
+    ]
+
+    list_filter = [
+        'create_date',
+        'article',
+        'parent',
+        'user',
+    ]
+
 
 admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(models.Article, ArticleAdmin)
