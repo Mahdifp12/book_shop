e1188d7112b45d748be8707ca08c93ac2447ec03
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-05-08 17:53:18 +0330

    CREATE & CHANGE : i'm created a new function for fill parent id & dynamicated parent id and article id

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,12 +1,18 @@
-function sendArticleComment() {
+function sendArticleComment(articleId) {
 
     var comment = $("#comment-text-article").val();
+    var parentId = $("#parent_id").val();
 
     $.get("/articles/add-article-comment", {
-        ArticleComment : comment,
-        ArticleId : 9,
-        ArticleParent : null
+        article_comment : comment,
+        article_id : articleId,
+        parent_id : parentId
     }).then(res => {
         console.log(res);
     });
+}
+
+function fillParentId(parentId){
+    $("#parent_id").val(parentId)
+    document.getElementById('comment_form').scrollIntoView({behavior: "smooth"});
 }
