7015dd1b9b53b1858a6c20abb429a9dba6437320
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-05-20 15:35:36 +0330

    CHANGE & REFACTOR : i'm added a new class based view for user panel

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class UserPanelDashboardPage(TemplateView):
+    template_name = "user_panel_module/user_panel_page.html"
