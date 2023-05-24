b7c8ea08f3224a635cb9fd880c813705b371a6c7
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-05-24 17:33:49 +0330

    CHANGE & ADD : i'm added a new url path for edit user profile

--- user_panel_module/urls.py
+++ user_panel_module/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.UserPanelDashboardPage.as_view(), name="user_panel_page"),
+    path('edit-profile', views.EditUserProfilePage.as_view(), name="edit_user_profie"),
 ]
