9ff8ac589342c68f7f50dc416dc3f9bfd86d36da
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-05-24 17:32:57 +0330

    CHANGE & ADD : i'm added a new function based view for component & class based view for edit user profile

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,8 +1,28 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
+
+from .forms import EditProfileModelForm
 
 
 # Create your views here.
 
 class UserPanelDashboardPage(TemplateView):
     template_name = "user_panel_module/user_panel_page.html"
+
+
+class EditUserProfilePage(View):
+    def get(self, request: HttpRequest):
+        edit_form = EditProfileModelForm()
+        context = {
+            "form": edit_form,
+        }
+        return render(request, 'user_panel_module/edit_profile_page.html', context)
+
+    def post(self, request: HttpRequest):
+        return render(request, 'user_panel_module/edit_profile_page.html', {})
+
+
+def user_panel_menu_component(request: HttpRequest):
+    return render(request, "components/user_panel_menu_component.html")
