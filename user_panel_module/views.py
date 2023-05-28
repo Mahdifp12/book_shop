d8a15a693a4d8a1e57b8796f8a8326ed080e30e7
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-05-28 15:39:44 +0330

    CHANGE & ADD : i'm seting POST and GET method

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
 
+from book_account.models import User
 from .forms import EditProfileModelForm
 
 
@@ -14,14 +15,25 @@ class UserPanelDashboardPage(TemplateView):
 
 class EditUserProfilePage(View):
     def get(self, request: HttpRequest):
-        edit_form = EditProfileModelForm()
+        current_user = User.objects.filter(id=request.user.id).first()
+        edit_form = EditProfileModelForm(instance=current_user)
         context = {
             "form": edit_form,
+            "current_user": current_user
         }
         return render(request, 'user_panel_module/edit_profile_page.html', context)
 
     def post(self, request: HttpRequest):
-        return render(request, 'user_panel_module/edit_profile_page.html', {})
+        current_user = User.objects.filter(id=request.user.id).first()
+        edit_form = EditProfileModelForm(request.POST, request.FILES, instance=current_user)
+        if edit_form.is_valid():
+            edit_form.save(commit=True)
+
+        context = {
+            "form": edit_form,
+            "current_user": current_user
+        }
+        return render(request, 'user_panel_module/edit_profile_page.html', context)
 
 
 def user_panel_menu_component(request: HttpRequest):
