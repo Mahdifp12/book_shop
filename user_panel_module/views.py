f5904723876676e49f13f05e47fdc8ad0043f86c
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-06-07 18:04:44 +0330

    CHANGE & ADD : i'm added a new class based view for change user password

--- user_panel_module/views.py
+++ user_panel_module/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import logout
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 
 from book_account.models import User
-from .forms import EditProfileModelForm
+from .forms import EditProfileModelForm, ChangePasswordForm
 
 
 # Create your views here.
@@ -34,6 +36,35 @@ class EditUserProfilePage(View):
             "current_user": current_user
         }
         return render(request, 'user_panel_module/edit_profile_page.html', context)
+
+
+class ChangePasswordPage(View):
+    def get(self, request: HttpRequest):
+        form = ChangePasswordForm()
+        context = {
+            "form": form
+        }
+        return render(request, 'user_panel_module/change_password_page.html', context)
+
+    def post(self, request: HttpRequest):
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            current_user = User.objects.filter(id=request.user.id).first()
+            current_password = form.cleaned_data.get("current_password")
+            is_correct_password = current_user.check_password(current_password)
+
+            if is_correct_password:
+                new_password = form.cleaned_data.get("password")
+                current_user.set_password(new_password)
+                current_user.save()
+                logout(request)
+                return redirect(reverse("login-page"))
+            else:
+                form.add_error('password', 'رمز عبور وارد شده اشتباه میباشد')
+        context = {
+            "form": form
+        }
+        return render(request, 'user_panel_module/change_password_page.html', context)
 
 
 def user_panel_menu_component(request: HttpRequest):
