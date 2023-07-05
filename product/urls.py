8f242dc232a8bf7bc447ed765c2aa136ea5ead6d
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-07-05 23:15:08 +0330

    CHANGE & ADD : i'm added a new URL for book categories component

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book-list'),
+    path('cat/<str:category>', views.BookListView.as_view(), name='book-categories'),
     path('book-favorite', views.BookFavorite.as_view(), name='book-favorite'),
     path('<slug:slug>', views.BookDetailView.as_view(), name='book-detail'),
 
