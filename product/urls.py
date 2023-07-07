e03983eaf504d9f7a45def33b02667993581be0f
Mahdifp12 <mahdifallahtpishe80@gmail.com>
2023-07-07 17:17:12 +0330

    REFACTORING

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book-list'),
     path('cat/<str:category>', views.BookListView.as_view(), name='book-categories'),
-    path('author/<str:authorname>', views.BookListView.as_view(), name='book-authors'),
+    path('author/<str:author_name>', views.BookListView.as_view(), name='book-authors'),
     path('book-favorite', views.BookFavorite.as_view(), name='book-favorite'),
     path('<slug:slug>', views.BookDetailView.as_view(), name='book-detail'),
 
